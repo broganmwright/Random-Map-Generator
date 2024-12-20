34e8080bfa22645964dd1029a5075c5ddd97b1e1
broganmwright <broganmwright@gmail.com>
2024-12-19 21:19:06 -0500

    syntax update/added additional info

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Directions:
 Go to https://editor.p5js.org/ and paste the code from sketch.js and press the play button to run the algorithm and generate a new map
 
 ![My Image](example.jpg)
+
+More information on Perlin noise and related topics:
+- https://en.wikipedia.org/wiki/Perlin_noise
+- https://en.wikipedia.org/wiki/Procedural_generation
+- https://en.wikipedia.org/wiki/Pseudorandomness
--- sketch.js
+++ sketch.js
@@ -1,21 +1,21 @@
 function setup() {
   createCanvas(400, 400);
-  let noiseScale = 1/125;
-  let water = color(72,100,212);
-  let shore = color(246,243,160);
-  let land = color(33,150,35);
+  let noiseScale = 1 / 125;
+  let water = color(72, 100, 212);
+  let shore = color(246, 243, 160);
+  let land = color(33, 150, 35);
   
-  for (let i=0; i<height; i++){
-    for (let j=0; j<width; j++){
-      let n = noise(i*noiseScale, j*noiseScale);
-      if (n<0.4){
-        set(i,j,water);
+  for (let i = 0; i < height; i++){
+    for (let j = 0; j < width; j++){
+      let n = noise(i * noiseScale, j * noiseScale);
+      if (n < 0.4){
+        set(i, j, water);
       }
-      else if (n<0.5){
-        set(i,j,shore);
+      else if (n < 0.5){
+        set(i, j, shore);
       }
-      else if (n>0.5){
-        set(i,j,land);
+      else if (n > 0.5){
+        set(i, j, land);
       }
     }
   }
